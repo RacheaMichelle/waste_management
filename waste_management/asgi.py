e1b32eceb1f9ee454ab9b8ed2b34aac8ee3820b4
RachealMichelle <rachealnannozi77@gmail.com>
2025-06-01 01:20:19 +0300

    Update asgi.py

--- waste_management/asgi.py
+++ waste_management/asgi.py
@@ -1,14 +1,17 @@
+# asgi.py
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from waste. routing import websocket_urlpatterns  # Make sure this import works
+import waste.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waste_management.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
+        URLRouter(
+            waste.routing.websocket_urlpatterns
+        )
     ),
 })
