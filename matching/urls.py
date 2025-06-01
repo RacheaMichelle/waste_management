85983285c8f2ea204a751a22f1b0fd3d550df419
RachealMichelle <rachealnannozi77@gmail.com>
2025-06-01 03:56:40 +0300

    Update urls.py

--- matching/urls.py
+++ matching/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path from . import views from django.conf import settings from django.conf.urls.static import static
+from django.urls import path 
+from . import views from django.conf import settings 
+from django.conf.urls.static import static
 
 urlpatterns = [ path('', views.matches, name='matches'), 
                 path('notifications/', views.notifications, name='notifications'), 
