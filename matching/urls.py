42bd9b82a248d59a4e13836dfddba181e22bcd85
RachealMichelle <rachealnannozi77@gmail.com>
2025-06-01 04:00:50 +0300

    Update urls.py

--- matching/urls.py
+++ matching/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [ path('', views.matches, name='matches'),
                 path('notifications/mark/int:notification_id/', views.mark_notification_read, name='mark_notification_read'), 
                 path('notifications/delete/int:notification_id/', views.delete_notification, name='delete_notification'), 
                 path('notifications/clear/', views.clear_all_notifications, name='clear_all_notifications'), 
-              ] += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
