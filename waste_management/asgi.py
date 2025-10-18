ad782b9fbf2366258a0eaf7ee44b57d38b87dc22
RachealMichelle <rachealnannozi77@gmail.com>
2025-10-18 03:14:59 +0300

    Update ASGI configuration for Vercel deployment
    Modified ASGI configuration to disable WebSockets for Vercel compatibility.

--- waste_management/asgi.py
+++ waste_management/asgi.py
@@ -1,17 +1,8 @@
-# asgi.py
+# waste_management/asgi.py
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import waste.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waste_management.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            waste.routing.websocket_urlpatterns
-        )
-    ),
-})
+# For Vercel, use HTTP only - disable WebSockets if they cause issues
+application = get_asgi_application()
